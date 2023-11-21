14e941a1c07049590ab9ace5fa7361fcd78ef70c
jacopaghera <123456789+jacopaghera@gmail.com>
2023-11-21 18:23:40 +0100

    made the functions that do the operations and return the right values in the display

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function operate(firstNumber, operator, secondNumber) {
         return subtract(firstNumber, secondNumber);
     }
     else if (operator === "*") {
-        return multiply (firstNumber, secondNumber);
+        return multiply(firstNumber, secondNumber);
     }
     else if (operator === "/") {
         return divide(firstNumber, secondNumber);
@@ -32,16 +32,38 @@ function operate(firstNumber, operator, secondNumber) {
 
 const numberButton = document.querySelectorAll(".number");
 const clearDisplay = document.querySelector(".clear");
-const display = document.querySelector('#display')
+const display = document.querySelector('#display');
+const operators = document.querySelectorAll(".op");
+const equal = document.querySelector(".equal");
+let firstNumber; let operator; let secondNumber;
+
 clearDisplay.addEventListener("click", () => {
-    display.innerText = ""
-})
+    display.innerText = "";
+});
+
 console.log(numberButton);
 
 numberButton.forEach((element) => {
     let buttonContent = element.textContent;
     element.addEventListener("click", () => {
-        let displayValue = document.querySelector("#display").innerText += buttonContent;
-        return(displayValue);
+    document.querySelector("#display").innerText += buttonContent;
     })
 });
+
+operators.forEach((element) => {
+    element.addEventListener('click', () => {
+    firstNumber = Number(display.textContent);
+    display.innerText = "";
+    operator = element.textContent;
+})
+});
+
+equal.addEventListener("click", () => {
+    secondNumber = Number(display.textContent);
+    if (secondNumber === 0 && operator === "/") {
+        display.innerText = "You divided by 0, no do!";
+    }
+    else {
+        display.innerText = operate(firstNumber, operator, secondNumber);
+    }
+})
